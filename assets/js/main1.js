81bade3b4319f34743e015ea63be17e569f56a52
Robinson <robinson0993@gmail.com>
2022-01-30 20:58:16 -0300

    agregando nuevamente scripts del ejercicio n1

--- assets/js/main1.js
+++ assets/js/main1.js
@@ -1,32 +1,56 @@
+function validar(nombre, asunto, mensaje) {
+    let pasamosLaValidacion = true;
+    document.querySelector(".resultado").innerHTML = "";
+    const patron = /[A-aZ-z]/gim;
+    const patron1 = /[A-aZ-z]/gim;
+    const patron2 = /[A-aZ-z]/gim;
 
-
-
-
-
-//  let form = document.getElementById("formulario");
-
-//  form.addEventListener("submit", function (e) {
-//      e.preventDefault();
-     
-//      let nombre = document.querySelector("#nombre").value;
-//      let asunto = document.querySelector("#asunto").value;
-//      let mensaje = document.querySelector("#mensaje").value;
-//      let resultado = validar(nombre, asunto, mensaje);
-
-//      console.log(nombre);
-//      console.log(asunto);
-//      console.log(mensaje);
-//      console.log(resultado);
-
-
-//      if (resultado == true) {
+    if (patron.test(nombre) == false) {
+        document.querySelector(".errorNombre").innerHTML = "El nombre es requerido.";
         
-//          document.querySelector("#mensajeenviado").innerHTML = "Mensaje Enviado con exito!!!";
-//          // alert("Enviar otro mensaje?");
-//          resultado = false;
-//          document.querySelector("#nombre").value="";
-//          document.querySelector("#asunto").value="";
-//          document.querySelector("#mensaje").value="";
-//          // window.location.reload() ;
-//      };
-//  });
+        pasamosLaValidacion = false;
+    } else {
+        document.querySelector(".errorNombre").innerHTML = "";
+        pasamosLaValidacion = true;
+    }
+
+    if (patron1.test(asunto) == false) {
+        document.querySelector(".errorAsunto").innerHTML = "El asunto es requerido.";
+        pasamosLaValidacion = false;
+    } else {
+        document.querySelector(".errorAsunto").innerHTML = "";
+        pasamosLaValidacion = true;
+    }
+
+    if (patron2.test(mensaje) == false) {
+        document.querySelector(".errorMensaje").innerHTML = "El mensaje es requerido.";
+        pasamosLaValidacion = false;
+    } else {
+        document.querySelector(".errorMensaje").innerHTML = "";
+        pasamosLaValidacion = true;
+    }
+    return pasamosLaValidacion;
+};
+
+
+ let form = document.getElementById("formulario");
+
+ form.addEventListener("submit", function (e) {
+     e.preventDefault();
+
+     
+     
+     let nombre = document.querySelector("#nombre").value;
+     let asunto = document.querySelector("#asunto").value;
+     let mensaje = document.querySelector("#mensaje").value;
+     let resultado = validar(nombre, asunto, mensaje);
+
+
+     if (resultado == true) {
+         document.querySelector(".resultado").innerHTML = "Mensaje Enviado con exito!!!";
+         document.querySelector("#nombre").value="";
+         document.querySelector("#asunto").value="";
+         document.querySelector("#mensaje").value="";
+
+     };
+ });
